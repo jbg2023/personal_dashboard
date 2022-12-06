5098e01a7044af47c72c96e956dcce46b4beeb17
jbg2023 <jbgates@protonmail.com>
2022-12-06 11:46:52 -0800

    Stocks displays top 5 stock news with likes

--- personal_dashboard/src/App.js
+++ personal_dashboard/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Quotes from './components/Quotes/Quotes';
+import Stocks from './components/Stocks/Stocks';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <div className='innerbox'>
         <div className='subboxsingle'>
           <h1>Stocks</h1>
+            <Stocks />
         </div>
       </div>
       <div className='innerbox'>
---
+++ personal_dashboard/src/components/Stocks/Stocks.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import './Stocks.css'
+
+function Stocks() {
+    const [ stocks, setStocks ] = useState(null)
+    
+
+    const showStocks = () => {
+       fetch('https://api.polygon.io/v2/reference/news?apiKey=aLriDEvFgjQLT7f3abtivxH60WMzReFo')
+            .then(response => response.json())
+            // .then(response => setStocks(response))
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+        }
+    useEffect(() => {
+        showStocks()
+    }, [])
+    if(!stocks){
+        return(
+            <p>Loading....</p>
+        )
+    }
+    return (
+        <div>
+            <h3>Top Stock News</h3>
+            <ul>
+                <li><a href={stocks.results[1].article_url}>{stocks.results[0].title}</a></li>
+                <li><a href={stocks.results[1].article_url}>{stocks.results[1].title}</a></li>
+                <li><a href={stocks.results[2].article_url}>{stocks.results[2].title}</a></li>
+                <li><a href={stocks.results[3].article_url}>{stocks.results[3].title}</a></li>
+                <li><a href={stocks.results[4].article_url}>{stocks.results[4].title}</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Stocks
