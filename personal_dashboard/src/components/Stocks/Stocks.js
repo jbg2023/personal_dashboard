b640e2c4f42cd23ea4704819d72c3edbc9216b0d
jbg2023 <jbgates@protonmail.com>
2022-12-06 13:18:13 -0800

    top 5 stock news links display in Stocks component

--- personal_dashboard/src/components/Stocks/Stocks.js
+++ personal_dashboard/src/components/Stocks/Stocks.js
@@ -3,13 +3,11 @@ import './Stocks.css'
 
 function Stocks() {
     const [ stocks, setStocks ] = useState(null)
-    
-
     const showStocks = () => {
        fetch('https://api.polygon.io/v2/reference/news?apiKey=aLriDEvFgjQLT7f3abtivxH60WMzReFo')
             .then(response => response.json())
-            // .then(response => setStocks(response))
-            .then(response => console.log(response))
+            .then(response => setStocks(response))
+            // .then(response => console.log(response))
             .catch(err => console.error(err));
         }
     useEffect(() => {
@@ -24,13 +22,14 @@ function Stocks() {
         <div>
             <h3>Top Stock News</h3>
             <ul>
-                <li><a href={stocks.results[1].article_url}>{stocks.results[0].title}</a></li>
-                <li><a href={stocks.results[1].article_url}>{stocks.results[1].title}</a></li>
-                <li><a href={stocks.results[2].article_url}>{stocks.results[2].title}</a></li>
-                <li><a href={stocks.results[3].article_url}>{stocks.results[3].title}</a></li>
-                <li><a href={stocks.results[4].article_url}>{stocks.results[4].title}</a></li>
+                <li><a href={stocks.results[0].article_url}>{stocks.results[0].title}</a></li><br/>
+                <li><a href={stocks.results[1].article_url}>{stocks.results[1].title}</a></li><br/>
+                <li><a href={stocks.results[2].article_url}>{stocks.results[2].title}</a></li><br/>
+                <li><a href={stocks.results[3].article_url}>{stocks.results[3].title}</a></li><br/>
+                <li><a href={stocks.results[4].article_url}>{stocks.results[4].title}</a></li><br/>
             </ul>
         </div>
     )
 }
+
 export default Stocks
