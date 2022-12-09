604b1200b1f1e96e75f841d1944dcfd4ee79a910
TimisCarrington <timiscarrington@gmail.com>
2022-12-09 11:17:23 -0800

    Weathere component now styled, and forecast has a toggle to display

--- personal_dashboard/src/components/Weather/Weather.js
+++ personal_dashboard/src/components/Weather/Weather.js
@@ -7,6 +7,26 @@ import React, { useState } from 'react';
 
 const Weather = (props) => {
     const [forecast, setForecast] = useState(true);
+
+    const customStyles = {
+        fontFamily:  'Helvetica, sans-serif',
+        gradientStart:  '#0181C265',
+        gradientMid:  '#04A7F965',
+        gradientEnd:  '#4BC4F765',
+        locationFontColor:  '#FFF',
+        todayTempFontColor:  '#FFF',
+        todayDateFontColor:  '#B5DEF4',
+        todayRangeFontColor:  '#B5DEF4',
+        todayDescFontColor:  '#B5DEF4',
+        todayInfoFontColor:  '#B5DEF4',
+        todayIconColor:  '#FFF',
+        forecastBackgroundColor:  '#439CEA65',
+        forecastSeparatorColor:  '#DDD',
+        forecastDateColor:  '#FFF',
+        forecastDescColor:  '#FFF',
+        forecastRangeColor:  '#FFF',
+        forecastIconColor:  '#FFF',
+    };
    
       const { data, isLoading, errorMessage } = useOpenWeather({
         key: '9493340bcdeabdb3bd334c664f543c59',
@@ -18,6 +38,7 @@ const Weather = (props) => {
       return (
         <div onClick={()=> setForecast(!forecast)}>
         <ReactWeather
+        theme={customStyles}
           isLoading={isLoading}
           errorMessage={errorMessage}
           data={data}
